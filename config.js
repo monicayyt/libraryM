22fbc1b3eaebacae429743e5fdceec20c0642a26
Meng Wang <Meng@LEE-168669.local>
2025-07-14 00:55:01 -0400

    add a jsonbin data store

---
+++ config.js
@@ -0,0 +1,10 @@
+   const firebaseConfig = {
+     apiKey: "your_api_key",
+     authDomain: "your_project.firebaseapp.com",
+     projectId: "your_project_id",
+     storageBucket: "your_project.appspot.com",
+     messagingSenderId: "123456789",
+     appId: "1:123456789:web:abcdef"
+   };
+
+export { firebaseConfig };
